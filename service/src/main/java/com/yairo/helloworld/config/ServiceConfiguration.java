9d0227cb611188d8b42b9657012de72e40e3d8a5
yairoffir <yairo87@users.noreply.github.com>
2023-02-26 14:18:49 +0200

    validations

--- service/src/main/java/com/yairo/helloworld/config/ServiceConfiguration.java
+++ service/src/main/java/com/yairo/helloworld/config/ServiceConfiguration.java
@@ -1,14 +1,18 @@
 package com.yairo.helloworld.config;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
 @Data
 @Configuration
 @ConfigurationProperties("hello-world")
+@Validated
 public class ServiceConfiguration {
 
+    @NotBlank
     private String serviceOwner;
 
 }
