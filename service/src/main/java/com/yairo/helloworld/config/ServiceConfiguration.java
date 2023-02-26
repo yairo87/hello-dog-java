9570bafb688aa0ba02305ca406235d6fa7ee5b8a
yairoffir <yairo87@users.noreply.github.com>
2023-02-26 13:28:53 +0200

    changing java version

--- service/src/main/java/com/yairo/helloworld/config/ServiceConfiguration.java
+++ service/src/main/java/com/yairo/helloworld/config/ServiceConfiguration.java
@@ -3,12 +3,10 @@ package com.yairo.helloworld.config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.validation.annotation.Validated;
 
 @Data
 @Configuration
 @ConfigurationProperties("hello-world")
-@Validated
 public class ServiceConfiguration {
 
     private String serviceOwner;
