5705ae927eccc28a0312015c59fb15d02106658b
yairoffir <yairo87@users.noreply.github.com>
2023-02-26 10:11:18 +0200

    spring updated to last 2.X
    swagger isnt working
    no validations
    spring is not yet 3.X

--- service/src/main/java/com/yairo/helloworld/config/ServiceConfiguration.java
+++ service/src/main/java/com/yairo/helloworld/config/ServiceConfiguration.java
@@ -5,15 +5,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
-import javax.validation.constraints.NotBlank;
-
 @Data
 @Configuration
 @ConfigurationProperties("hello-world")
 @Validated
 public class ServiceConfiguration {
 
-    @NotBlank
     private String serviceOwner;
 
 }
